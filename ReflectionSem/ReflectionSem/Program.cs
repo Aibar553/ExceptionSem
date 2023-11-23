93a30346f9deb86cc95347768406154cf46dddb8
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-11-23 21:39:05 +0600

    Add files via upload

---
+++ ReflectionSem/ReflectionSem/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace ReflectionSem
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class CustomNameAttribute : Attribute
+    {
+        public string CustomFieldName { get; }
+        public CustomNameAttribute(string customFieldName)
+        {
+            CustomFieldName = customFieldName;
+        }
+    }
+    public static class SerializationExtensions
+    {
+        public static string ObjectToString(this object obj)
+        {
+            var properties = obj.GetType().GetFields()
+                .ToDictionary(
+                field => GetCustomFieldName(field) ?? field.Name,
+                field => field.GetValue(obj)?.ToString() ?? "null"
+                );
+            return string.Join(", ", properties.Select(kv => $"{kv.Key}:{kv.Value}"));
+        }
+        public static void StringToObject(this object obj, string data)
+        {
+            var properties = obj.GetType().GetFields()
+                .ToDictionary(field => GetCustomFieldName(field) ?? field.Name);
+
+            var keyValuePairs = data.Split(',').Select(kv => kv.Trim().Split(':'));
+            foreach (var pair in keyValuePairs)
+            {
+                var propertyName = pair[0];
+                var propertyValue = pair.Length > 1 ? pair[1] : null;
+                if (properties.TryGetValue(propertyName, out var fieldInfo))
+                {
+                    var fieldValue = Convert.ChangeType(propertyValue, fieldInfo.FieldType);
+                    fieldInfo.SetValue(obj, fieldValue);
+                }
+            }
+        }
+        private static string GetCustomFieldName(FieldInfo fieldInfo)
+        {
+            var attribute = fieldInfo.GetCustomAttribute<CustomNameAttribute>();
+            return attribute?.CustomFieldName;
+        }
+    }
+    class MyClass
+    {
+        [CustomName("CustomFieldName")]
+        public int I = 0;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var obj = new MyClass();
+            var serialized = obj.ObjectToString();
+            Console.WriteLine(serialized);
+
+            var newObj = new MyClass();
+            newObj.StringToObject(serialized);
+            Console.WriteLine(newObj.I);
+        }
+    }
+}
